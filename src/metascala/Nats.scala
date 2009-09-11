eeb3af852162c89396a1d5a85efd4e6e86b6ab5b
mayhem <mayhem@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-09-11 16:28:40 +0000

    git-svn-id: http://svn.assembla.com/svn/metascala@11 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/metascala/Nats.scala
+++ src/metascala/Nats.scala
@@ -16,6 +16,8 @@ object Nats {
     type AddType = Nat
     type Accept[N <: NatVisitor] <: N#ResultType
     type Eq[N <: Nat] <: Bool
+
+    def toInt : Int
   }
   
   final class _0 extends Nat {
@@ -23,13 +25,17 @@ object Nats {
     type Is0 = True
     type Add[N <: Nat] = N
     type Accept[N <: NatVisitor] = N#Visit0
+
+    def toInt = 0
   }
   
-  final class Succ[P <: Nat] extends Nat {
+  final case class Succ[P <: Nat](toInt : Int) extends Nat {
     type Pre = P
     type Is0 = False
     type Add[N <: Nat] = Succ[P#Add[N]]
     type Accept[N <: NatVisitor] = N#VisitSucc[P]
+
+    def +[N <: Nat](n : N) : Add[N] = Succ[P#Add[N]](toInt + n.toInt)
   }
   
   type _1 = Succ[_0]
@@ -44,17 +50,19 @@ object Nats {
   type _10 = Succ[_9]
   
   val _0 = new _0
-  val _1 = new _1
-  val _2 = new _2
-  val _3 = new _3
-  val _4 = new _4
-  val _5 = new _5
-  val _6 = new _6
-  val _7 = new _7
-  val _8 = new _8
-  val _9 = new _9
-  val _10 = new _10
+  val _1 = new _1(1)
+  val _2 = new _2(2)
+  val _3 = new _3(3)
+  val _4 = new _4(4)
+  val _5 = new _5(5)
+  val _6 = new _6(6)
+  val _7 = new _7(7)
+  val _8 = new _8(8)
+  val _9 = new _9(9)
+  val _10 = new _10(10)
 
   implicit val _0ToInt = TypeToValue[_0, Int](0)
   implicit def succToInt[P <: Nat](implicit v : TypeToValue[P, Int]) = TypeToValue[Succ[P], Int](1 + v.value)
+
+  def toInt[T <: Nat](v : T)(implicit fn : TypeToValue[T, Int]) = fn()
 }

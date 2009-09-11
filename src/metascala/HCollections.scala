cc2700198d9584ecfb0c338ac08280a4b488c3ae
mayhem <mayhem@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-09-11 16:09:56 +0000

    git-svn-id: http://svn.assembla.com/svn/metascala@10 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/metascala/HCollections.scala
@@ -0,0 +1,19 @@
+package metascala
+
+object HCollections {
+  import Nats._
+  import TLists._
+
+  trait Tuple[L <: TList] {
+    type This[L <: TList] <: Tuple[L]
+
+    def ::[T](v : T) : This[T :: L]
+    def :::[L2 <: TList](l : This[L2]) : This[L2#Append[L]]
+    def apply[N <: Nat](n : N) : L#Nth[N]
+    def reverse : This[L#Reverse]
+    def removeNth[N <: Nat](n : N) : This[L#RemoveNth[N]]
+    def insert[N <: Nat, E](n : N, elem : E): This[L#Insert[N, E]]
+    def replaceSameType[N <: Nat, E](n : N, elem : E) : This[L]
+  }
+
+}

4ab0d7ed971a770bf73766ab8ca5fa37bf448e3e
mayhem <mayhem@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-09-12 09:07:44 +0000

    .
    git-svn-id: http://svn.assembla.com/svn/metascala@15 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/metascala/example/HArrayExample.scala
+++ src/metascala/example/HArrayExample.scala
@@ -6,28 +6,28 @@ object HArrayExample {
   import metascala.Utils._
 
   // Create a HArray of an Int, Boolean and a pair
-  val l1 = 10 :: true :: (10.1, "Hello") :: HArrayNil
+  val a1 = 10 :: true :: (10.1, "Hello") :: HArrayNil
 
   // Extract the second element, note that the element type
   // information is preserved and we can safely perform a
   // boolean and operation
-  val b = l1(_1) && false
+  val b = a1(_1) && false
 
   // Create another HArray, note the use of an operator in
   // the type expression
-  val l2 = harray(1.1, "string", false)
+  val a2 = harray(1.1, "string", false)
 
   // Replace the second element in the list, it used to
   // be a String, but now it's an Int
-  val l3 = l2.removeNth(_1).insert(_1, 14)
+  val a3 = a2.removeNth(_1).insert(_1, 14)
 
   // Type information preserved, we can use an Int operation
   // on the element
-  val i = l2(_1) / 2
+  val i = a3(_1) / 2
 
   // Append l2 to l1
-  val l4 = l1 ::: l2
+  val a4 = a1 ::: a2
 
   // Statically check that the length of l4 is 6
-  type T = Equal[_6, l4.Length]
+  type T = Equal[_6, a4.Length]
 }
